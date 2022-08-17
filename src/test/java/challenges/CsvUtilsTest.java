2ccd62f5279b9e4acd33deafe603eb485d71088f
Priyank Shah <priyank.shah217@gmail.com>
2022-08-17 23:46:06 +0530

    added more test cases

--- src/test/java/challenges/CsvUtilsTest.java
+++ src/test/java/challenges/CsvUtilsTest.java
@@ -13,11 +13,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CsvUtilsTest {
 
     @Test
-    public void getListOfData() throws IOException {
+    public void shouldShowCommonRows() throws IOException {
         File input1 = new File("src/main/resources/file1.csv");
         File input2 = new File("src/main/resources/file2.csv");
         CsvUtils csvUtils = new CsvUtils();
         List<User> commonData = csvUtils.getCommonData(input1, input2);
         assertThat(commonData).hasSize(2);
+    }
+
+    @Test
+    public void shouldNotAnyRows() throws IOException {
+        File input1 = new File("src/main/resources/file3.csv");
+        File input2 = new File("src/main/resources/file4.csv");
+        CsvUtils csvUtils = new CsvUtils();
+        List<User> commonData = csvUtils.getCommonData(input1, input2);
+        assertThat(commonData).hasSize(0);
+    }
+
+    @Test
+    public void shouldShowAllMatchingRows() throws IOException {
+        File input1 = new File("src/main/resources/file1.csv");
+        File input2 = new File("src/main/resources/file3.csv");
+        CsvUtils csvUtils = new CsvUtils();
+        List<User> commonData = csvUtils.getCommonData(input1, input2);
+        assertThat(commonData).hasSize(3);
     }
 }
